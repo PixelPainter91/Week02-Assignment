07585bf1df82ba815a937e79260b7216968df3fa
Rory Tanton <rory_tanton@hotmail.com>
2025-11-27 15:39:38 +0000

    chore: removed navbar in redesign, added sudo code to be built

---
+++ app.js
@@ -0,0 +1,15 @@
+//TODO: Create thumbnail images, so the user can click on them and see them in a fullscreen format
+
+//step1: store image data
+ const imageData = [
+    {
+    imageName:"value",
+    imageSrc: "value",
+    imageAlt: "value",
+ }
+]
+//step2: create thumbnail images
+ function createThumbnails(){
+    
+ }
+//step3: create fullscreen images
