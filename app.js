f0e6f56cd0ec25fb69cab1c80d57cddfacbc0e49
Rory Tanton <rory_tanton@hotmail.com>
2025-11-28 11:43:11 +0000

    Problem: image height on fullscreen images/image optimisation. finished js code, now styling with css.

--- app.js
+++ app.js
@@ -3,13 +3,79 @@
 //step1: store image data
  const imageData = [
     {
-    imageName:"value",
-    imageSrc: "value",
-    imageAlt: "value",
- }
+    imageName:"nwAdd",
+    imageSrc: "./assets/nailwardrobeadvert.jpg",
+    imageAlt: `A woman’s hand with pink nail polish on a pink background. The image includes text: “Sort your stash — with Nail Wardrobe. Upload > Filter > Keep track.”`,
+ },
+ {
+    imageName:"nwOrganised",
+    imageSrc: "./assets/organised.png",
+    imageAlt: `A black android phone on a pink background with an image gallery showing different colour nail swatches. Text above reads "The simple way to get organised. Catalog your collection"`,
+ },
+{
+    imageName:"nwUpload",
+    imageSrc: "./assets/upload.png",
+    imageAlt: `A black android phone on a pink background with an image gallery showing an upload symbol with the users photo gallery open, showing nail colour swatches. Text below reads "Upload: use the in app camera to upload an existing image.`,
+ },
+ {
+    imageName:"nwFilter",
+    imageSrc: "./assets/filter.png",
+    imageAlt: `A black android phone on a pink background with an image gallery showing a pink nail colour swatch and upload options: Name, Colour Group, Colour, Colour Code, Brand, Type, Finish`,
+ },
+ {
+    imageName:"nwCommunityDriven",
+    imageSrc: "./assets/communityDriven.png",
+    imageAlt: `A black Android phone on a pink background showing an empty nail polish bottle logo and the text ‘Nail Wardrobe.’ Above the phone, text reads: ‘Community driven: Your feedback makes Nail Wardrobe work for you.`,
+ },
+ {
+    imageName:"nwkeepTrack",
+    imageSrc: "./assets/keepTrack.png",
+    imageAlt: `A black Android phone on a pink background showing the Nail Wardrobe gallery with different nail colour swatches. On the right side are solid-colour nail polish bottle icons used to sort the gallery, and across the top are categories for tracking polish. The image includes the text: "Keep Track: Less time looking, knowing what you have in a tap."`,
+ },
+ 
 ]
 //step2: create thumbnail images
  function createThumbnails(){
-    
+    //select the DOM element (thumbnail-container) to contain my thumbnail
+    const thumbnailContainer = document.getElementById("thumbnail-container");
+    //for loop
+    //loop: task
+    for (let i = 0 ; i< imageData.length; i++){
+    //-creat img element 
+    const img = document.createElement("img");
+    //update the src and alt attributes of the img elemet to 
+    //match those in the array 
+    img.src =imageData[i].imageSrc;
+    img.alt =imageData[i].imageAlt;
+    //-give each img a className (img.className)
+    img.className = "thumbnail";
+    //- add an event listener to each image --> the event handler of this imagesis tje function you write to create the fullscreen images
+    img.addEventListener("click", function(){
+        createFullscreenImages(this.src, this.alt);
+    });
+    //--append the created images to the thumbnail-container
+    thumbnailContainer.appendChild(img);
+    }
  }
+
 //step3: create fullscreen images
+function createFullscreenImages(src, alt){
+    //this is the event handler
+    // select the fullscreen-container
+    const fullscreenContainer = document.getElementById("fullscreen-container");
+    //delete the current fullscreen image
+    fullscreenContainer.innerHTML = "";
+    // fullscreenContainer.innerHTML = "" or null
+    //create image
+    const fullImg= document.createElement("img");
+    fullImg.src = src;
+    fullImg.alt = alt;
+    //update its values 
+    //add className for styling
+    fullImg.className = "fullscreen-image"
+    //append image to container
+    fullscreenContainer.appendChild(fullImg);
+}
+document.addEventListener("DOMContentLoaded", function(){
+    createThumbnails();
+});
